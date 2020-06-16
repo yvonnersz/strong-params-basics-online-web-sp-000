fbe6c53851e6a0cac5a70bdc1632e6dec4db2371
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-16 03:59:58 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,17 +11,17 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
-	def create
-	  @post = Post.new(params["post"])
-	  @post.save
-	  redirect_to post_path(@post)
-	end
-
-	def update
-	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
-	  redirect_to post_path(@post)
-	end
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :description))
+    @post.save
+    redirect_to post_path(@post)
+  end
+   
+  def update
+    @post = Post.find(params[:id])
+    @post.update(params.require(:post).permit(:title))
+    redirect_to post_path(@post)
+  end
 
 	def edit
 	  @post = Post.find(params[:id])
