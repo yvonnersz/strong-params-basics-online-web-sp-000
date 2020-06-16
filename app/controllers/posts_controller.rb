7b27eab59e3ed9b4e9902c1f48a2e16ad9a5e19b
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-16 04:00:01 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
     @post.save
     redirect_to post_path(@post)
   end
-   
+
   def update
     @post = Post.find(params[:id])
     @post.update(params.require(:post).permit(:title))
